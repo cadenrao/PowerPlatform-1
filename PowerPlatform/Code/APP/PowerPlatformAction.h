7ff0a368403c295335863c4edd0f62d10a6db45c
zoewhence <zoewhence@foxmail.com>
2020-10-13 15:17:53 +0800

    modified zhangmodify

--- PowerPlatform/Code/APP/PowerPlatformAction.h
+++ PowerPlatform/Code/APP/PowerPlatformAction.h
@@ -1,13 +1,21 @@
 #ifndef __POWERPLATFORMACTION_H
 #define __POWERPLATFORMACTION_H
 
-#include "ReMoterKEY.h"
-#include "OUTPUT.h"
-#include "sys.h"
-#include "PWM.h"
-#include "DAC.h"
 
+#include "OUTPUT.h"//执行的动作锁定赢得输出口
+#include "sys.h"//位带操作支持、单片机头文件引入
+#include "PWM.h"//比例输出支持
+#include "DAC.h"//模拟量输出
 
+//函数命名规则： PP_动作（PP => PowerPlatform）
+
+void PP_StayStill(void);
+void PP_SpotTurnLeft(void);
+void PP_SpotTurnRight(void);
+void PP_GoForward(void);
+void PP_Go_TurnLeft(void);
+void PP_Go_TurnRight(void);
+////
 
 
 
