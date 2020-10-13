2015d9c66093c0d209016efb33c3e81bc11e73b2
zoewhence <zoewhence@foxmail.com>
2020-10-13 15:17:53 +0800

    modify

--- PowerPlatform/Code/APP/PowerPlatformAction.h
+++ PowerPlatform/Code/APP/PowerPlatformAction.h
@@ -1,11 +1,11 @@
 #ifndef __POWERPLATFORMACTION_H
 #define __POWERPLATFORMACTION_H
 
-#include "ReMoterKEY.h"
-#include "OUTPUT.h"
-#include "sys.h"
-#include "PWM.h"
-#include "DAC.h"
+
+#include "OUTPUT.h"//执行的动作锁定赢得输出口
+#include "sys.h"//位带操作支持、单片机头文件引入
+#include "PWM.h"//比例输出支持
+#include "DAC.h"//模拟量输出
 
 
 
