3a87a8249851e6ce1daa599cfd378c7da4222d58
zoewhence <zoewhence@foxmail.com>
2020-10-13 17:15:46 +0800

    加入forxunhuan

--- PowerPlatform/Code/main.c
+++ PowerPlatform/Code/main.c
@@ -24,7 +24,13 @@ int main()
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置NVIC中断分组2：2位抢占优先级，2位响应优先级
 	MY_EXTI_Init();//初始化中断
 	delay_init();
+
+	//for (size_t i = 0; i < length; i++)
+	//{
+
+	//}
 	while(1){}
+
 
 
 
